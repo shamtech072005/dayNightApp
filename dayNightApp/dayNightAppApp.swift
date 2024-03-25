3b518ed81f399bdee8584090d1ed7b1952460064
shamtech072005 <shamedward2@gmail.com>
2024-03-25 10:19:14 +0530

    Initial Commit

---
+++ dayNightApp/dayNightAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  dayNightAppApp.swift
+//  dayNightApp
+//
+//  Created by shamtech07 on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct dayNightAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
